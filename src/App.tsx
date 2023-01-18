a6d7921420f1d4ee85d82c08aafb4805d7c2dbbd
happo31 <happo31@gmail.com>
2023-01-18 23:36:51 +0900

    fix: template コード削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 import { Navigate, Outlet, Route, Routes } from "react-router-dom";
 import MainLayout from "./components/Layouts/MainLayout";
 import Top from "./screens/Top/Top";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
     <Routes>
       <Route
