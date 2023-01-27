48c37e0be6cfff67deca7d70a5677ea9bb632845
happo31 <happo31@gmail.com>
2023-01-28 00:03:30 +0900

    feat: 環境変数追加

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TWITTER_API_KEY: string;
+  readonly VITE_APP_TWITTER_API_KEY_SECRET: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMeta;
+}
